913149dd18ec6f1052dd2ceb2100a3e7c752ca4a
ikmz <ikmz0104.official@gmail.com>
2024-08-09 17:44:06 +0900

    chore

--- contract/scripts/deployPortfolioFactory.ts
+++ contract/scripts/deployPortfolioFactory.ts
@@ -4,6 +4,7 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 // npx hardhat run scripts/deployPortfolioFactory.ts --network base-sepolia
 // npx hardhat verify --network base-sepolia 0x83b23Fc8B2D004Dc613c845b8242783B3B3efa0B
 // https://sepolia.etherscan.io/address/0xCA9310e085cD711dff293a73681A9d8d00e6A22c
+// https://sepolia.basescan.org/address/0x83b23Fc8B2D004Dc613c845b8242783B3B3efa0B
 
 async function main() {
   const PortfolioFactoryFactory = await ethers.getContractFactory("PortfolioFactory");
