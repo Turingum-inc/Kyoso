03e79925bc6f9311cd362b2eacc0356e097f3e1c
ikmz <ikmz0104.official@gmail.com>
2024-08-09 17:08:47 +0900

    etherscanがbase-sepoliaに対応していない

--- contract/scripts/deployPortfolioFactory.ts
+++ contract/scripts/deployPortfolioFactory.ts
@@ -1,15 +1,15 @@
 const { ethers } = require("hardhat");
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-// npx hardhat run scripts/deployPortfolioFactory.ts --network sepolia
-// npx hardhat verify --network sepolia 0xCA9310e085cD711dff293a73681A9d8d00e6A22c
+// npx hardhat run scripts/deployPortfolioFactory.ts --network base-sepolia
+// npx hardhat verify --network base-sepolia 0x83b23Fc8B2D004Dc613c845b8242783B3B3efa0B
 // https://sepolia.etherscan.io/address/0xCA9310e085cD711dff293a73681A9d8d00e6A22c
 
 async function main() {
   const PortfolioFactoryFactory = await ethers.getContractFactory("PortfolioFactory");
   const portfolioFactory = await PortfolioFactoryFactory.deploy();
 
-  console.log("PortfolioFactory deployed to:", `https://sepolia.etherscan.io/address/${portfolioFactory.target}`);
+  console.log("PortfolioFactory deployed to:", `https://sepolia.basescan.org/address/${portfolioFactory.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
